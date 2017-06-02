95dc08499fe4efea28f2b27a12d6d62ab9d54630
Kieu Anh Dung <kadung@tma.com.vn>
2017-06-02 16:42:31 +0700

    js

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+// This is a comment
+
+/* This is 
+   a block comments */
+
+/* 
+	JavaScript provides seven different data types which are undefined, null, boolean, string, symbol, number, and object.
+	We tell JavaScript to create or declare a variable by putting the keyword var in front of it, as "var name_of_variable;"
+	Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
+*/
+	var x,y,z;
+	
+/*	
+*/
+
+
+
+/*	
+*/
+
+
+/*	
+*/
+
+
+/*	
+*/
+
+
+/*	
+*/
+
+
+/*	
+*/
